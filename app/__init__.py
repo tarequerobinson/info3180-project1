e15df2ecc5c40fb49df56e98559dcf08ec31bf76
tarequerobinson <tarequerobinson@gmail.com>
2024-03-06 17:29:28 -0500

    Exercise 1 complete

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate  
 from .config import Config
-# import flask migrate here
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
-# Instantiate Flask-Migrate library here
+
+# Instantiate Flask-Migrate here
+migrate = Migrate(app, db)
 
 # Flask-Login login manager
 login_manager = LoginManager()
--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from . import db
 
-
+from flask_login import UserMixin
 class UserProfile(db.Model):
     # You can use this to change the table name. The default convention is to use
     # the class name. In this case a class name of UserProfile would create a
