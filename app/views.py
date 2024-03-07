c7055dc9e73c3c145bb9b50cc993723e6eda9f58
tarequerobinson <tarequerobinson@gmail.com>
2024-03-06 19:17:02 -0500

    Exercise 4 complete

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
 from app.forms import LoginForm
+from werkzeug.security import check_password_hash
 
 
 ###
@@ -41,23 +42,29 @@ def upload():
 def login():
     form = LoginForm()
 
-    # change this to actually validate the entire form submission
-    # and not just one field
-    if form.username.data:
+    if form.validate_on_submit():
         # Get the username and password values from the form.
+        username = form.username.data
+        password = form.password.data
 
         # Using your model, query database for a user based on the username
         # and password submitted. Remember you need to compare the password hash.
         # You will need to import the appropriate function to do so.
-        # Then store the result of that query to a `user` variable so it can be
-        # passed to the login_user() method below.
+        user = UserProfile.query.filter_by(username=username).first()
 
-        # Gets user id, load into session
-        login_user(user)
+        if user and check_password_hash(user.password , password):
+            # Login the user
+            login_user(user)
 
-        # Remember to flash a message to the user
-        return redirect(url_for("home"))  # The user should be redirected to the upload form instead
-    return render_template("login.html", form=form)
+            # Remember to flash a message to the user
+            flash('Login successful!', 'success')
+
+            return redirect(url_for('upload'))  # Redirect to the home page
+        else:
+            # Invalid username or password
+            flash('Invalid username or password', 'error')
+
+    return render_template('login.html', form=form)
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
